725c4a8f34b345b32a89cda2997bab44b437833f
ruben7405 <rubenxtlv075@gmail.com>
2025-08-21 10:55:26 -0500

    Ruben s2

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("Iniciando asistencia DMP");
+        AsistenciaApplication.main(args);
+    }
+}
